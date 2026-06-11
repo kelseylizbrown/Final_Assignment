d774bfe00e311d3be51a12018d833f31d6ba45a9
kelseylizbrown <kelsey.brown2@kellogg.northwestern.edu>
2026-06-11 06:50:40 +0000

    user auth

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,16 +1,26 @@
 class EntriesController < ApplicationController
 
   def new
+    @current_user = User.find_by({ "id" => session["user_id"] })
+    if @current_user == nil
+      redirect_to "/login"
+    end
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    @current_user = User.find_by({ "id" => session["user_id"] })
+    if @current_user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = @current_user["id"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    else
+      redirect_to "/login"
+    end
   end
 
 end
