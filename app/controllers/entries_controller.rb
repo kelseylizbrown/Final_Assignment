3539ebcf3355bd15dd7bc8709f99f69a547c169b
kelseylizbrown <kelsey.brown2@kellogg.northwestern.edu>
2026-06-12 04:20:01 +0000

    image attachment

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,20 +7,21 @@ class EntriesController < ApplicationController
     end
   end
 
-  def create
-    @current_user = User.find_by({ "id" => session["user_id"] })
-    if @current_user != nil
-      @entry = Entry.new
-      @entry["title"] = params["title"]
-      @entry["description"] = params["description"]
-      @entry["occurred_on"] = params["occurred_on"]
-      @entry["place_id"] = params["place_id"]
-      @entry["user_id"] = @current_user["id"]
-      @entry.save
-      redirect_to "/places/#{@entry["place_id"]}"
-    else
-      redirect_to "/login"
-    end
+def create
+  @current_user = User.find_by({ "id" => session["user_id"] })
+  if @current_user != nil
+    @entry = Entry.new
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["occurred_on"] = params["occurred_on"]
+    @entry["place_id"] = params["place_id"]
+    @entry["user_id"] = @current_user["id"]
+    @entry.save
+    @entry.uploaded_image.attach(params["uploaded_image"])
+    redirect_to "/places/#{@entry["place_id"]}"
+  else
+    redirect_to "/login"
   end
+end
 
 end
